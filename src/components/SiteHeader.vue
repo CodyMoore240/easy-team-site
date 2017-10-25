c20f37d3800631d55efd812e20461491da0b9556
Cody Moore <codymoore240@gmail.com>
2017-10-24 22:26:58 -0400

    Homepage and logo are configurable from the DB

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,18 +1,36 @@
-<template lang="html">
-  <div class="headerWrapper">
-    <div class="logo">
+<template lang='html'>
+  <div class='headerWrapper'>
+    <div class='logo' v-bind:style='styleObject'>
       Logo
     </div>
   </div>
 </template>
 
 <script>
-export default {
+// import firebase
+import firebase from 'firebase'
 
+export default {
+  data () {
+    return {
+      // create a style object that will come directly form firebase
+      styleObject: {}
+    }
+  },
+  beforeMount () {
+    // get the data from firebase and i guess we will make this real time for now
+    firebase.firestore().collection('pages').doc('theme').onSnapshot((doc) => {
+      this.styleObject = doc.data().logo
+    })
+  },
+  beforeDestroy () {
+    // remove the firebase listener
+    firebase.firestore().collection('pages').doc('theme').onSnapshot((doc) => {})
+  }
 }
 </script>
 
-<style scoped lang="scss">
+<style scoped lang='scss'>
   .headerWrapper {
     background-color: #46628A;
     padding: 10px;
